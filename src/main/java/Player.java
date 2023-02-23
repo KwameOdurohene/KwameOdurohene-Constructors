87dfa51211ced4539569cf7d24d766f4b437b412
KwameOdurohene <kodurohene@gmail.com>
2023-02-23 03:56:57 +0000

    completed boolean operation

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -31,6 +31,10 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
+        this.playerName = playerName;
+        this.level = 1;
+        System.out.println(playerName);    
+
 
     }
 
@@ -48,6 +52,10 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
+        this.playerName = playerName;
+        this.level = level;
+        System.out.println(playerName);
+        System.out.println(level);     
 
     }
 
